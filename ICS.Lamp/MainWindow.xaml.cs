d3825db180dc1f471d9704635b66af99af7446c7
rockiez <simpi@foxmail.com>
2015-11-15 23:46:59 +0800

    fix bugs

--- ICS.Lamp/MainWindow.xaml.cs
+++ ICS.Lamp/MainWindow.xaml.cs
@@ -22,17 +22,17 @@ namespace ICS.Lamp
 
     public class onoffbutton : Window
     {
-        public Image lampimg, onoffimg;
-        public byte[] on, off;
-        public string errormessage;
-        public bool bs = false;
+        Image lampimg, onoffimg;
+        byte[] oncode, offcode;
+        string errormessage;
+        bool bs = false;
 
-        public onoffbutton(Image lampimg, Image onoffimg, byte[] on, byte[] off, string errormessage)
+        public onoffbutton(Image lampimg, Image onoffimg, byte[] oncode, byte[] offcode, string errormessage)
         {
-            this.lampimg = lampimg;
+            this.            lampimg = lampimg;
             this.onoffimg = onoffimg;
-            this.on = on;
-            this.off = off;
+            this.oncode = oncode;
+            this.offcode = offcode;
             this.errormessage = errormessage;
         }
 
@@ -50,7 +50,7 @@ namespace ICS.Lamp
                 MessageBox.Show(bret.ResultMessage);
             else
             {
-                bool adamResult = adam.OnOff(bs ? off : on);
+                bool adamResult = adam.OnOff(bs ? offcode : oncode); 
                 if (adamResult)
                 {
                     bs = !bs;
@@ -76,9 +76,6 @@ namespace ICS.Lamp
         }
     }
 
-    /// <summary>
-    /// /
-    /// </summary>
 
     public partial class MainWindow : Window
     {
@@ -87,37 +84,22 @@ namespace ICS.Lamp
             InitializeComponent();
         }
 
-        private void Image_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
-        {
-        offonstarts();
-        }
+        private void Image_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e){offonstarts();}
+        private void Image_MouseLeftButtonUp_2(object sender, MouseButtonEventArgs e){offonstarts();}
+        private void Image_MouseLeftButtonUp_3(object sender, MouseButtonEventArgs e){offonstartc();}
+        private void Image_MouseLeftButtonUp_4(object sender, MouseButtonEventArgs e){offonstartc();}
 
-        private void Image_MouseLeftButtonUp_2(object sender, MouseButtonEventArgs e)
-        {
-        offonstarts();
-        }
-
-        private void Image_MouseLeftButtonUp_3(object sender, MouseButtonEventArgs e)
-        {offonstartc();
-        }
-
-        private void Image_MouseLeftButtonUp_4(object sender, MouseButtonEventArgs e)
-        {offonstartc();
-        }
-
-
-        public void offonstartc(){
-        
         byte[] con = new byte[] { 0x01, 0x05, 0x00, 0x11, 0xFF, 0x00, 0xDC, 0x3F };
         byte[] coff = new byte[] { 0x01, 0x05, 0x00, 0x11, 0x00, 0x00, 0x9D, 0xCF };
+        byte[] son = new byte[] { 0x01, 0x05, 0x00, 0x12, 0xFF, 0x00, 0x2C, 0x3F };
+        byte[] soff = new byte[] { 0x01, 0x05, 0x00, 0x12, 0x00, 0x00, 0x6D, 0xCF };
+
+        public void offonstartc(){
         onoffbutton cbutton = new onoffbutton(imgc, imgcs, con,coff,"操作楼道灯失败");
         cbutton.onoffmain();
         }
 
-        public void offonstarts()
-        {
-            byte[] son = new byte[] { 0x01, 0x05, 0x00, 0x12, 0xFF, 0x00, 0x2C, 0x3F };
-            byte[] soff = new byte[] { 0x01, 0x05, 0x00, 0x12, 0x00, 0x00, 0x6D, 0xCF };
+        public void offonstarts(){
             onoffbutton sbutton = new onoffbutton(imgs, imgss, son, soff, "操作街道等失败");
             sbutton.onoffmain();
         }
